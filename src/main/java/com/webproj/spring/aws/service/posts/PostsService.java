18db69d03b4c75005cb71b856e8e5fae5a1444ae
com-HP <d0296989@gmail.com>
2024-12-02 00:52:33 +0900

    Create/Read/Update api

---
+++ src/main/java/com/webproj/spring/aws/service/posts/PostsService.java
@@ -0,0 +1,39 @@
+package com.webproj.spring.aws.service.posts;
+
+import com.webproj.spring.aws.domain.posts.Posts;
+import com.webproj.spring.aws.domain.posts.PostsRepository;
+import com.webproj.spring.aws.web.dto.PostsResponseDto;
+import com.webproj.spring.aws.web.dto.PostsSaveRequestDto;
+import com.webproj.spring.aws.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
+    }
+}
