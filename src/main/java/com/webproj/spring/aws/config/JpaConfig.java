7293628132c4a120e8280fc4a6ee8222545e4bc0
com-HP <d0296989@gmail.com>
2024-12-04 23:16:20 +0900

    Security applied to existing tests-2

---
+++ src/main/java/com/webproj/spring/aws/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.webproj.spring.aws.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
